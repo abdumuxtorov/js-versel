97d60662db7a4e0951560f83d0e6f43fed614df2
abdumuxtorov <abdumuxtorov0209@gmail.com>
2023-12-17 21:12:08 +0300

    intitla commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var salom = alert('Assalomu alaykum')
+
+var isim = prompt('Iltimos ismingizni kiriting')
+console.log(isim);
+
+var telefon = prompt("Telefon Nomeringizni kiriting")
+console.log(telefon);
+
+var yosh = prompt('Yoshingizni kiriting')
+console.log(yosh);
